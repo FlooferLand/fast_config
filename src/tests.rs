be006bd9a56c683c433ca66caaaad5638e5a27ec
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2023-01-23 02:22:16 +0200

    Added a heavier more advanced test

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,33 @@
 use log::LevelFilter;
-use crate::{Config, ConfigOptions};
+use crate::{Config, ConfigOptions, ConfigFormat};
 use serde::{Serialize, Deserialize};
+
+// Sub-data
+#[derive(Serialize, Deserialize)]
+pub struct SubData {
+    pub string: String,
+    pub unsigned: u64,
+    pub boolean: bool
+}
+
+// Data
+#[derive(Serialize, Deserialize)]
+pub struct MyData {
+    pub number: i32,
+    pub subdata: SubData
+}
+impl Default for MyData {
+    fn default() -> Self {
+        Self {
+            number: 20,
+            subdata: SubData {
+                string:   format!("Joe Mama"),
+                unsigned: 400,
+                boolean:  true
+            }
+        }
+    }
+}
 
 #[test]
 fn run() {
@@ -9,37 +36,9 @@ fn run() {
         .filter_level(LevelFilter::Info)
         .init();
 
-    // Sub-data
-    #[derive(Serialize, Deserialize)]
-    pub struct SubData {
-        pub string: String,
-        pub unsigned: u64,
-        pub boolean: bool
-    }
-
-    // Data
-    #[derive(Serialize, Deserialize)]
-    pub struct MyData {
-        pub number: i32,
-        pub subdata: SubData
-    }
-
-    impl Default for MyData {
-        fn default() -> Self {
-            Self {
-                number: 20,
-                subdata: SubData {
-                    string:   format!("Joe Mama"),
-                    unsigned: 400,
-                    boolean:  true
-                }
-            }
-        }
-    }
-
     // Creating options
     let options = ConfigOptions {
-        pretty: false,
+        pretty: true,
         ..Default::default()
     };
 
@@ -69,12 +68,108 @@ fn run() {
         assert_eq!(config.data.subdata.boolean, default.subdata.boolean);
     }
 
+    // Advanced test
+    if std::env::var("ADVANCED_TEST").is_ok() {
+        advanced_test();
+    }
+
     // Cleanup
-    std::thread::sleep(std::time::Duration::from_millis(30));
+    std::thread::sleep(std::time::Duration::from_millis(20));
     match std::fs::remove_dir_all("./config/") {
         Ok(_) => {}
         Err(e) => {
             log::error!("{e}");
+        }
+    }
+}
+
+// TODO: Advanced test code should be refactored as it looks incredibly messy.
+//       Doesn't really matter due to the fact it's a test, though
+fn advanced_test() {
+    pub enum FormatFinder<'a> {
+        GuessExtension(&'a str),
+        Config(crate::ConfigFormat),
+        Feature
+    }
+    
+    pub struct Case<'a> {
+        pub format_finder: FormatFinder<'a>,
+        pub pretty: bool
+    }
+    impl<'a> Case<'a> {
+        pub fn new(format_finder: FormatFinder<'a>, pretty: bool) -> Self {
+            Self { format_finder, pretty }
+        }
+    }
+
+    // Adding all different possible cases
+    // <!> Could probably be made slightly faster by being moved into an array via a macro
+    let extensions = ["json", "toml", "yaml"];
+    let mut cases = Vec::with_capacity((extensions.len() * 3) * 2);
+    let mut pretty = false;
+    for _ in 0..2 {
+        for ext in extensions {
+            cases.push(Case::new(
+                FormatFinder::GuessExtension(ext),
+                pretty
+            ));
+            cases.push(Case::new(
+                FormatFinder::Config(ConfigFormat::from_extension(ext)),
+                pretty
+            ));
+            cases.push(Case::new(
+                FormatFinder::Feature,
+                pretty
+            ));
+        }
+        pretty = !pretty;
+    }
+
+    // Automated case-based tests
+    for case in cases {
+        let mut path = String::from("./config/advtestconfig");
+        let mut format = ConfigOptions::default().format;
+        match case.format_finder {
+            FormatFinder::GuessExtension(ext) => {
+                path += format!(".{ext}").as_str();
+            },
+            FormatFinder::Config(fmt) => {
+                format = fmt;
+            },
+            FormatFinder::Feature => {}
+        };
+
+        // Creating options
+        let options = ConfigOptions {
+            pretty: case.pretty,
+            format,
+            ..Default::default()
+        };
+
+        // Creating the config and saving it
+        {
+            let mut config = Config::<MyData>::from_options(&path, options, MyData::default());
+            config.data.number = i32::MAX;
+            config.save();
+        }
+
+        // Reading from that config + assertions
+        {
+            // Test data
+            let data = MyData {
+                number: 0,
+                subdata: SubData {
+                    string:   String::new(),
+                    unsigned: 0,
+                    boolean:  false
+                }
+            };
+            let config = Config::<MyData>::new(&path, data);
+            let default = MyData::default();
+            assert_eq!(config.data.number, i32::MAX);
+            assert_eq!(config.data.subdata.string, default.subdata.string);
+            assert_eq!(config.data.subdata.unsigned, default.subdata.unsigned);
+            assert_eq!(config.data.subdata.boolean, default.subdata.boolean);
         }
     }
 }
